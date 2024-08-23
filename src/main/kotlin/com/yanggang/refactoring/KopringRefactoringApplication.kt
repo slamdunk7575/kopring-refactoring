704b8719bafb9045430ef15c8bce43227c120ce1
slamdunk7575 <slamdunk7575@gmail.com>
2024-08-23 13:12:50 +0900

    (feat) init commit

---
+++ src/main/kotlin/com/yanggang/refactoring/KopringRefactoringApplication.kt
@@ -0,0 +1,11 @@
+package com.yanggang.refactoring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringRefactoringApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringRefactoringApplication>(*args)
+}
