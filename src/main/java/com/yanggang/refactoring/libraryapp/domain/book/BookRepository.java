bde184fa502bc677e4858ebaef6d3bb8df35c92a
slamdunk7575 <slamdunk7575@gmail.com>
2024-08-27 04:29:16 +0900

    (feat) 코틀린으로 리팩토링전 JAVA 코드 추가 (Book)

---
+++ src/main/java/com/yanggang/refactoring/libraryapp/domain/book/BookRepository.java
@@ -0,0 +1,11 @@
+package com.yanggang.refactoring.libraryapp.domain.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findByName(String bookName);
+
+}
---
+++ src/main/java/com/yanggang/refactoring/libraryapp/service/book/BookService.java
@@ -0,0 +1,56 @@
+package com.yanggang.refactoring.libraryapp.service.book;
+
+import com.yanggang.refactoring.libraryapp.domain.book.Book;
+import com.yanggang.refactoring.libraryapp.domain.book.BookRepository;
+import com.yanggang.refactoring.libraryapp.domain.user.User;
+import com.yanggang.refactoring.libraryapp.domain.user.UserRepository;
+import com.yanggang.refactoring.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository;
+import com.yanggang.refactoring.libraryapp.dto.book.request.BookLoanRequest;
+import com.yanggang.refactoring.libraryapp.dto.book.request.BookRequest;
+import com.yanggang.refactoring.libraryapp.dto.book.request.BookReturnRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+    private final UserRepository userRepository;
+    private final UserLoanHistoryRepository userLoanHistoryRepository;
+
+    public BookService(
+            BookRepository bookRepository,
+            UserRepository userRepository,
+            UserLoanHistoryRepository userLoanHistoryRepository
+    ) {
+        this.bookRepository = bookRepository;
+        this.userRepository = userRepository;
+        this.userLoanHistoryRepository = userLoanHistoryRepository;
+    }
+
+    @Transactional
+    public void saveBook(BookRequest request) {
+        Book newBook = new Book(request.getName());
+        bookRepository.save(newBook);
+    }
+
+    @Transactional
+    public void loanBook(BookLoanRequest request) {
+        Book book = bookRepository.findByName(request.getBookName())
+                .orElseThrow(IllegalArgumentException::new);
+        if (userLoanHistoryRepository.findByBookNameAndIsReturn(request.getBookName(), false) != null) {
+            throw new IllegalArgumentException("진작 대출되어 있는 책입니다");
+        }
+
+        User user = userRepository.findByName(request.getUserName())
+                .orElseThrow(IllegalArgumentException::new);
+        user.loanBook(book);
+    }
+
+    @Transactional
+    public void returnBook(BookReturnRequest request) {
+        User user = userRepository.findByName(request.getUserName()).orElseThrow(IllegalArgumentException::new);
+        user.returnBook(request.getBookName());
+    }
+
+}
