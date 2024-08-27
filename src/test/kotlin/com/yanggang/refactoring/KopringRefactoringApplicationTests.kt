d0fb884b23326aaa9c321eb310fe3312df4166fb
slamdunk7575 <slamdunk7575@gmail.com>
2024-08-27 19:07:13 +0900

    (test) 전체 테스트

--- src/test/kotlin/com/yanggang/refactoring/KopringRefactoringApplicationTests.kt
+++ src/test/kotlin/com/yanggang/refactoring/KopringRefactoringApplicationTests.kt
@@ -1,13 +1,7 @@
 package com.yanggang.refactoring
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class KopringRefactoringApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
 }
